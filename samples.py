b514e9c10c4a9af1967d24984a899483b0e67e77
NACHIROU <nachirousomad24@gmail.com>
2024-03-08 16:58:01 +0100

    all tests about debit

---
+++ samples.py
@@ -0,0 +1,23 @@
+beans_tokens = {
+    "debit_data": {
+        "account": "somad.nachirou@ai.trellix.io",
+        "rule": "rule_0fo6ofiv",
+        "quantity": 12,
+        "description": "Debited for a $12 discount on order #12345.",
+        "uid": "order_12345_debit",
+    },
+    "debit_canceled_data": {
+        "id": "dr_05pfx3hfidmooctu8tafgynbaul4pd6u",
+        "object": "debit",
+        "beans": 12,
+        "account": "acc_0ueqlqh9npn35v",
+        "rule": "rule_0fo6ofiv",
+        "description": "Debited for a $12 discount on order #12345.",
+        "status": "committed",
+        "failure_code": None,
+        "failure_message": None,
+        "uid": "order_12345_debit",
+        "created": "2024-03-05T10:47:35.646271Z",
+        "updated": "2024-03-05T10:47:35.646290Z",
+    },
+}
